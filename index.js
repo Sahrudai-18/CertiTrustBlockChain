daa62cdc993d9d649ae874d0dcfeb29e83615c21
Sahrudai-18 <sahrudaireddy49@example.com>
2023-10-26 19:58:25 -0500

    firsty

---
+++ index.js
@@ -0,0 +1,7 @@
+function showDashboard()
+	{
+		let nav = document.querySelector('.navigation');
+		let toggle = document.querySelector('.toggle');
+		nav.classList.toggle('active');
+		toggle.classList.toggle('active');
+	}
